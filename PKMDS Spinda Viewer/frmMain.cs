74a5ef47fb07791a7773c3c017fc27c5603c06cd
Michael Bond <codemonkey8510@icloud.com>
2022-05-13 16:38:59 -0400

    Upgrades

--- PKMDS Spinda Viewer/frmMain.cs
+++ PKMDS Spinda Viewer/frmMain.cs
@@ -1,49 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PKMDS_CS;
+using System;
 using System.Windows.Forms;
-using PKMDS_CS;
+
 namespace PKMDS_Spinda_Viewer
 {
     public partial class frmMain : Form
     {
-        public frmMain()
-        {
-            InitializeComponent();
-        }
-        private void numPID_ValueChanged(object sender, EventArgs e)
-        {
-            UpdateSpindaSprite();
-        }
-        private void chkShiny_CheckedChanged(object sender, EventArgs e)
-        {
-            UpdateSpindaSprite();
-        }
+        public frmMain() => InitializeComponent();
+
+        private void numPID_ValueChanged(object sender, EventArgs e) => UpdateSpindaSprite();
+
+        private void chkShiny_CheckedChanged(object sender, EventArgs e) => UpdateSpindaSprite();
+
         private void UpdateSpindaSprite()
         {
-            pbSpriteNormal.Image = PKMDS.GetSpindaSprite(((UInt32)(numPID.Value)), false);
-            pbSpriteShiny.Image = PKMDS.GetSpindaSprite(((UInt32)(numPID.Value)), true);
+            pbSpriteNormal.Image = PKMDS.GetSpindaSprite((uint)numPID.Value, false);
+            pbSpriteShiny.Image = PKMDS.GetSpindaSprite((uint)numPID.Value, true);
         }
-        private void chkHex_CheckedChanged(object sender, EventArgs e)
+
+        private void chkHex_CheckedChanged(object sender, EventArgs e) => numPID.Hexadecimal = chkHex.Checked;
+
+        private uint GetRandom()
         {
-            numPID.Hexadecimal = chkHex.Checked;
-        }
-        private UInt32 GetRandom()
-        {
-            UInt32 rand = 0;
-            Random rnd = new Random(DateTime.Now.Millisecond);
-            rand = (UInt32)rnd.Next(Int32.MinValue, Int32.MaxValue);
+            var rnd = new Random(DateTime.Now.Millisecond);
+            var rand = (uint)rnd.Next(int.MinValue, int.MaxValue);
             return rand;
         }
-        private void btnRandom_Click(object sender, EventArgs e)
-        {
-            numPID.Value = GetRandom();
-        }
+
+        private void btnRandom_Click(object sender, EventArgs e) => numPID.Value = GetRandom();
+
         private void frmMain_Load(object sender, EventArgs e)
         {
             msMain.Visible = false;
